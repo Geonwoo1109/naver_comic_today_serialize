646a1d2d2a0275e48fd87c6f5f8e1e44f34eba6d
Hibot <hui1601@naver.com>
2020-09-14 23:33:18 +0900

    Update naver.js

--- naver.js
+++ naver.js
@@ -6,8 +6,6 @@ importClass(java.util.Base64);
 importClass(org.jsoup.Jsoup);
 const _String = java.lang.String;
 
-let cArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
-
 function toBase64(buffer) {
     return Base64.getEncoder().encodeToString(buffer);
 }
